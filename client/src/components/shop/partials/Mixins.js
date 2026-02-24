330e9822c9a67e583e9ef1561c49c071146b4800
subratkumarpadhy4 <padhysubratkumar7@gmail.com>
2026-02-24 20:23:07 +0530

    fix: add null safety checks for empty cart in Mixins.js

--- client/src/components/shop/partials/Mixins.js
+++ client/src/components/shop/partials/Mixins.js
@@ -1,30 +1,36 @@
 export const subTotal = (id, price) => {
   let subTotalCost = 0;
   let carts = JSON.parse(localStorage.getItem("cart"));
-  carts.forEach((item) => {
-    if (item.id === id) {
-      subTotalCost = item.quantitiy * price;
-    }
-  });
+  if (carts) {
+    carts.forEach((item) => {
+      if (item.id === id) {
+        subTotalCost = item.quantitiy * price;
+      }
+    });
+  }
   return subTotalCost;
 };
 
 export const quantity = (id) => {
   let product = 0;
   let carts = JSON.parse(localStorage.getItem("cart"));
-  carts.forEach((item) => {
-    if (item.id === id) {
-      product = item.quantitiy;
-    }
-  });
+  if (carts) {
+    carts.forEach((item) => {
+      if (item.id === id) {
+        product = item.quantitiy;
+      }
+    });
+  }
   return product;
 };
 
 export const totalCost = () => {
   let totalCost = 0;
   let carts = JSON.parse(localStorage.getItem("cart"));
-  carts.forEach((item) => {
-    totalCost += item.quantitiy * item.price;
-  });
+  if (carts) {
+    carts.forEach((item) => {
+      totalCost += item.quantitiy * item.price;
+    });
+  }
   return totalCost;
 };
