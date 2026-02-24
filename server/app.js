d48ea3bc8e32d3718947e885c8b980e3495f17a5
subratkumarpadhy4 <padhysubratkumar7@gmail.com>
2026-02-24 22:46:54 +0530

    fix: increase express body size limit to 50MB for uploading base64 images

--- server/app.js
+++ server/app.js
@@ -58,8 +58,8 @@ app.use(async (req, res, next) => {
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 // Routes
 app.get("/", (req, res) => res.status(200).send("OK"));
